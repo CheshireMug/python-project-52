c26694be513f2ec34fb5999f9af2ed0a9b28c1d5
Игнат <mrOerg@gmail.com>
2025-12-19 18:28:56 +0300

    corrected backslash error

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -62,8 +62,10 @@ class RegistrationForm(UserCreationForm):
             'class': 'form-control',
             'placeholder': 'Пароль',
         }),
-        help_text='<ul><li>Ваш пароль должен содержать' + \
-        ' как минимум 3 символа.</li></ul>'
+        help_text=(
+            '<ul><li>Ваш пароль должен содержать'
+            ' как минимум 3 символа.</li></ul>'
+        )
     )
 
     password2 = forms.CharField(
@@ -127,8 +129,10 @@ class UserUpdateForm(UserCreationForm):
             'placeholder': 'Пароль',
         }),
         required=False,
-        help_text='<ul><li>Ваш пароль должен содержать' + \
+        help_text=(
+            '<ul><li>Ваш пароль должен содержать'
             ' как минимум 3 символа.</li></ul>'
+            )
     )
 
     password2 = forms.CharField(
